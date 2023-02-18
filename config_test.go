73695df939209b803c27fd5addae869f97cd2f32
Hidetake Iwata <int128@gmail.com>
2023-02-18 17:57:35 +0900

    Add test

--- config_test.go
+++ config_test.go
@@ -2,27 +2,22 @@ package oauth2githubapp
 
 import (
 	"context"
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+	"time"
 )
 
-type fakeGitHubAppInstallationTokenAPI struct{}
-
-func (f fakeGitHubAppInstallationTokenAPI) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Method should be POST", http.StatusMethodNotAllowed)
-		return
-	}
-	authorizationHeader := r.Header.Get("authorization")
-	if authorizationHeader == "" {
-		http.Error(w, "No authorization header", http.StatusUnauthorized)
-		return
-	}
-	// https://docs.github.com/en/rest/apps/apps?apiVersion=2022-11-28#create-an-installation-access-token-for-an-app
-	w.WriteHeader(201)
-	_, _ = io.WriteString(w, `{
+// https://docs.github.com/en/rest/apps/apps?apiVersion=2022-11-28#create-an-installation-access-token-for-an-app
+const fakeAccessToken = `{
   "token": "ghs_16C7e42F292c6912E7710c838347Ae178B4a",
   "expires_at": "2016-07-11T22:14:10Z",
   "permissions": {
@@ -31,7 +26,65 @@ func (f fakeGitHubAppInstallationTokenAPI) ServeHTTP(w http.ResponseWriter, r *h
   },
   "repository_selection": "selected",
   "repositories": []
-}`)
+}`
+
+type fakeCreateInstallationAccessToken struct {
+	t         *testing.T
+	publicKey *rsa.PublicKey
+	appID     string
+}
+
+func (f fakeCreateInstallationAccessToken) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		f.t.Errorf("method wants POST but was %s", r.Method)
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	authorizationHeader := r.Header.Get("authorization")
+	if err := f.authorize(authorizationHeader); err != nil {
+		f.t.Logf("authorization header: %s", authorizationHeader)
+		f.t.Errorf("invalid authorization header: %s", err)
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
+		return
+	}
+	w.WriteHeader(201)
+	if _, err := io.WriteString(w, fakeAccessToken); err != nil {
+		f.t.Errorf("write error: %s", err)
+	}
+}
+
+func (f fakeCreateInstallationAccessToken) authorize(header string) error {
+	if !strings.HasPrefix(header, "Bearer ") {
+		return fmt.Errorf("header wants Bearer token")
+	}
+	token := strings.TrimPrefix(header, "Bearer ")
+	parts := strings.Split(token, ".")
+	if len(parts) != 3 {
+		return fmt.Errorf("token wants 3 parts but was %d parts", len(parts))
+	}
+	hashed := sha256.Sum256([]byte(parts[0] + "." + parts[1]))
+	sign, err := base64.RawURLEncoding.DecodeString(parts[2])
+	if err != nil {
+		return fmt.Errorf("invalid base64 of sign: %w", err)
+	}
+	if err := rsa.VerifyPKCS1v15(f.publicKey, crypto.SHA256, hashed[:], sign); err != nil {
+		return fmt.Errorf("verify error: %s", err)
+	}
+
+	payload, err := base64.RawURLEncoding.DecodeString(parts[1])
+	if err != nil {
+		return fmt.Errorf("invalid base64 of payload: %w", err)
+	}
+	var payloadToken struct {
+		Issuer string `json:"iss"`
+	}
+	if err := json.Unmarshal(payload, &payloadToken); err != nil {
+		return fmt.Errorf("invalid payload: %w", err)
+	}
+	if f.appID != payloadToken.Issuer {
+		return fmt.Errorf("issuer wants %s but was %s", f.appID, payloadToken.Issuer)
+	}
+	return nil
 }
 
 func TestConfig_Token(t *testing.T) {
@@ -41,7 +94,11 @@ func TestConfig_Token(t *testing.T) {
 	}
 
 	fakeGitHub := http.NewServeMux()
-	fakeGitHub.Handle("/app/installations/MyInstallationID/access_tokens", fakeGitHubAppInstallationTokenAPI{})
+	fakeGitHub.Handle("/app/installations/MyInstallationID/access_tokens", fakeCreateInstallationAccessToken{
+		t:         t,
+		publicKey: &privateKey.PublicKey,
+		appID:     "MyAppID",
+	})
 	fakeServer := httptest.NewServer(fakeGitHub)
 	defer fakeServer.Close()
 
@@ -56,6 +113,12 @@ func TestConfig_Token(t *testing.T) {
 		t.Fatalf("token error: %s", err)
 	}
 	if w := "ghs_16C7e42F292c6912E7710c838347Ae178B4a"; token.AccessToken != w {
-		t.Errorf("token wants %s but was %s", w, token.AccessToken)
+		t.Errorf("token.AccessToken wants %s but was %s", w, token.AccessToken)
+	}
+	if w := time.Date(2016, 7, 11, 22, 14, 10, 0, time.UTC); token.Expiry != w {
+		t.Errorf("token.Expiry wants %s but was %s", w, token.Expiry)
+	}
+	if w := "token"; token.TokenType != w {
+		t.Errorf("token.TokenType wants %s but was %s", w, token.AccessToken)
 	}
 }
